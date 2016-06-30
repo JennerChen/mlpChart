9666a8ba248a91b77c7323d891f7b5c2d1f8dcc7
Qing Zhang <zhangqing332@live.com>
2016-06-30 23:57:41 +0800

    update lib files

--- src/util.js
+++ src/util.js
@@ -4,4 +4,5 @@ var events = require('./utils/events');
 var elementHelper = require('./utils/elementHelper');
 var foramt = require('./utils/format');
 var i18n = require('./i18n/i18n');
+
 module.exports = _.extend({}, common, apiHelper, events, elementHelper, foramt, i18n);
